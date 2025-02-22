32fdc43e145e39ec3ff04a3cbda3ec92ea34f520
Yevhen <132243427+Zhendrey@users.noreply.github.com>
2025-02-21 21:02:02 -0800

    Adding and applying viteStaticCopy at vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,28 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
     base: '/space-tourism-deploy/',
+    plugins: [
+        viteStaticCopy({
+        targets: [
+            {
+            src: 'src/json/destination.json',
+            dest: ''
+            },
+            {
+            src: 'src/json/crew.json',
+            dest: ''
+            },
+            {
+            src: 'src/json/technology.json',
+            dest: ''
+            },
+            {
+                src: 'assets/**/*', // Copy all images from assets directory
+                dest: 'assets' // Maintain the directory structure in the dist folder
+            }
+        ]
+        })
+        ]
 })
